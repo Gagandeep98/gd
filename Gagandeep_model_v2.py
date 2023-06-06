53348e21955bbbbdeaf1e80862d951b96b4f6f1c
Gagandeep98 <gagan.ssms98@gmail.com>
2023-06-06 01:40:10 -0400

    adding new file

---
+++ Gagandeep_model_v2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Import the necessary libraries:
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+
+# In[2]:
+
+
+#Load the dataset using the provided URL:
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+data = pd.read_csv(url, header=None)
+
+
+# In[3]:
+
+
+#Separate the features (X) and the target variable (y):
+
+X = data.iloc[:, 2:]  # Select all columns starting from the 3rd column as features
+y = data.iloc[:, 1]   # Select the 2nd column as the target variable
+
+
+# In[4]:
+
+
+#Split the dataset into training and testing sets:
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# In[5]:
+
+
+#Create an instance of the Random Forest classifier:
+
+model = RandomForestClassifier(random_state=42)
+
+
+# In[6]:
+
+
+#Fit the model to the training data:
+
+model.fit(X_train, y_train)
+
+
+# In[7]:
+
+
+#Make predictions on the testing set:
+
+y_pred = model.predict(X_test)
+
+
+# In[9]:
+
+
+#Evaluate the model's performance
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+
+
+# In[ ]:
+
+
+
+
